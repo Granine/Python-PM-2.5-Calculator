5fe2f121e8cdb280fa80adeb6e19e39136915795
Granine <guanzheng.huang@hotmail.com>
2023-03-24 23:19:57 -0700

    Add test for successful request (WIP)

--- pm25_test/integration_test.py
+++ pm25_test/integration_test.py
@@ -24,3 +24,12 @@ def test_pm_25_class_no_token():
         pass
     else:
         assert "Class did not error out without token" == 0
+        
+def test_pm_25_general():
+    '''General pm25 test
+    '''
+    if "waqi_token" not in os.environ.keys(): 
+        raise Warning("waqi_token not set in environmental variable")
+    pm25_calc = pm25.PM25_Calculator(*random_area_1())
+    assert type(pm25_calc) == pm25.PM25_Calculator
+    assert type(pm25_calc.get_average_pm25(1, 0))
