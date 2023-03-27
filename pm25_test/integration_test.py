f1e208c18de9e98c27026395ae9b22ed25692dc8
Granine <guanzheng.huang@hotmail.com>
2023-03-27 13:44:05 -0700

    Added new test for success request

--- pm25_test/integration_test.py
+++ pm25_test/integration_test.py
@@ -2,33 +2,41 @@ import sys
 import os
 sys.path.append(f"{__file__}/../..")
 import pm25
+from unittest import mock
 
 def random_area_1():
     # A random area with stations
-    return (54, -354, 55, -354.5)
+    return (39,116,-40,116.01)
 
+@mock.patch.dict(os.environ, {"waqi_token": "DUMMY"})
 def test_pm_25_class_initialization():
     '''Basic test to ensure class can be initialized
     '''
     os.environ["waqi_token"] = "DUMMY"
     pm25_calc = pm25.PM25_Calculator(*random_area_1())
     assert type(pm25_calc) == pm25.PM25_Calculator
-    
+
+@mock.patch.dict(os.environ, {"waqi_token": "DUMMY"})
 def test_pm_25_class_no_token():
     '''Make sure class fails initialization if no token detected
     '''
-    if "waqi_token" in os.environ.keys(): del(os.environ["waqi_token"])
+    save_token = ""
+    if "waqi_token" in os.environ.keys(): 
+        save_token = os.environ["waqi_token"]
+        del(os.environ["waqi_token"])
     try:
         pm25_calc = pm25.PM25_Calculator(*random_area_1())
     except AttributeError:
         pass
     else:
         assert "Class did not error out without token" == 0
+    if save_token:
+        os.environ["waqi_token"] = save_token
         
 def test_pm_25_general():
     '''General pm25 test
     '''
-    if "waqi_token" not in os.environ.keys(): 
+    if "waqi_token" not in os.environ: 
         raise Warning("waqi_token not set in environmental variable")
     pm25_calc = pm25.PM25_Calculator(*random_area_1())
     assert type(pm25_calc) == pm25.PM25_Calculator
